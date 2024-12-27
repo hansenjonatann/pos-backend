d55b55afe86c04c474e5f0349b86fd5a35700baa
hansenjonatann <hansenjonatann@gmail.com>
2024-12-27 09:04:37 +0700

    feat: config

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,15 @@
-import {app} from '../src/app';
+import { app } from '../src/app';
 
 const server = app;
 
 // Handler untuk Vercel
 export default async (req: any, res: any) => {
-  await server.ready();
-  server.server.emit('request', req, res);
+  try {
+    await server.ready();
+    server.server.emit('request', req, res);
+  } catch (err) {
+    console.error('Error in serverless function:', err);
+    res.statusCode = 500;
+    res.end('Internal Server Error');
+  }
 };
