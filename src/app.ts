6ef8bd062cde4ac8ed9233369d1f7fee24c65a5d
hansenjonatann <hansenjonatann@gmail.com>
2024-12-27 09:11:16 +0700

    feat: update config app.ts and remove the api.ts

--- src/app.ts
+++ src/app.ts
@@ -21,5 +21,11 @@ app.register(unitRoutes , {prefix: '/units'})
 app.register(transactionRoutes , {prefix: '/transactions'})
 
 
-
+app.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+    app.log.info(`Server listening at ${address}`);
+  });
   
